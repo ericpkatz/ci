f28e725f7e69b9758c3459f12c1210eddf0e8ea5
Eric P. Katz <ericpkatz@gmail.com>
2015-11-09 21:28:27 -0500

    test

---
+++ test/server/api/products_test.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var request = require('supertest-as-promised');
+var app = require('../../../app');
+
+describe('/products', function(){
+  describe('/get', function(){
+    describe('with no products', function(){
+      it('returns an empty array', function(){
+        return request(app).get('/api/products')
+          .expect(200)
+          .then(function(res){
+            assert(res.body.length == 0);
+          });
+      });
+    });
+  });
+});
