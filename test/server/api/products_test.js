92883084a5b59ab3e43d6b1c084a0a987be79608
Eric P. Katz <ericpkatz@gmail.com>
2015-11-14 10:49:09 -0500

    api returns two items

--- test/server/api/products_test.js
+++ test/server/api/products_test.js
@@ -5,11 +5,11 @@ var app = require('../../../app');
 describe('/products', function(){
   describe('/get', function(){
     describe('with no products', function(){
-      it('returns an empty array', function(){
+      it('returns two items', function(){
         return request(app).get('/api/products')
           .expect(200)
           .then(function(res){
-            assert(res.body.length == 1);
+            assert(res.body.length == 2);
           });
       });
     });
